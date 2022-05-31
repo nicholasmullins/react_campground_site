4b8b0721bfc33dda50a967606e519fda884669f8
Nicholas Mullins <nicholasmullins@gmail.com>
2022-05-31 16:25:29 +0300

    campsitesSlice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React from 'react';
+import Header from './components/Header';
+import CampsitesList from './features/campsites/CampsitesList.js';
+import Footer from './components/Footer';
 import './App.css';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color='primary' sticky='top' expand='md'>
-        <Container>
-          <NavbarBrand href='/'>
-              <img src={NucampLogo} alt='nucamp logo' />
-          </NavbarBrand>
-        </Container>
-      </Navbar>
-      I'm ready for the workshop!
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
