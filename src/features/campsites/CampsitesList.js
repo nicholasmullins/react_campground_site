1ab4b5ea08008177e1c2ecaffb394d6b707c48b9
Nicholas Mullins <nicholasmullins@gmail.com>
2022-08-03 20:54:46 +0200

    Moved CampSiteDetail off of the CampsiteDirectoryPage and changed it to where when a campsite in the directory is clicked on it routes it to it's own campsite.id number and each campsite has it's own detail page

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -2,7 +2,7 @@ import { Row, Col } from 'reactstrap';
 import CampsiteCard from './CampsiteCard';
 import { selectAllCampsites } from './campsitesSlice'
 
-const CampsitesList = ({ setCampsiteId }) => {
+const CampsitesList = () => {
 
     const campsites = selectAllCampsites();
 
@@ -14,7 +14,7 @@ const CampsitesList = ({ setCampsiteId }) => {
                         md ='5' 
                         className='m-4' 
                         key={campsite.id}
-                        onClick={() => setCampsiteId(campsite.id)}>
+                    >
                         <CampsiteCard campsite={campsite} />
                     </Col>
                 );
