e6ed39a03b9043e2a326b7cb60d6cda89dad8a9a
Nicholas Mullins <nicholasmullins@gmail.com>
2022-08-03 20:29:24 +0200

    Homepage Display. Created DipslayList, DisplayCard and edited the Homepage.js to include DisplayList component

--- src/features/display/DisplayCard.js
+++ src/features/display/DisplayCard.js
@@ -1,13 +1,16 @@
-import { Card, Carding, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-const DisplayCard = ({item}) => {
+const DisplayCard = ({ item }) => {
     const { image, name, description } = item;
     return (
         <Card>
             <CardImg src={image} alt={name} />
             <CardBody>
-                <CardTitle>{description}</CardTitle>
+                <CardTitle>{name}</CardTitle>
+                <CardText>{description}</CardText>
             </CardBody>
         </Card>
     )
 }
+
+export default DisplayCard;
